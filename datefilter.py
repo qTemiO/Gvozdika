12e0d71b832bf91fad4ad9e4d3daf96390ef66e4
Your Carma <74972003+YourCarma@users.noreply.github.com>
2022-03-11 11:46:59 +0300

    Update 2 datefilter.py
    1) Написал функцию, создающая список дат из заданного диапазона
    2) Ввел итерацию списка необходимых чатов
    3) Выходными данными являются списки типа: [ [Имя пользователя, дата публикации, текст сообщения], ...]

--- datefilter.py
+++ datefilter.py
@@ -1,13 +1,38 @@
 from telethon import TelegramClient
+import datetime
+from datetime import timedelta
 
-# Use your own values from my.telegram.org
 api_id = 15316406
 api_hash = 'ad62ac5526adf8d79b7d4b5d26f7e93e'
 session_name = 'Igor'
 client = TelegramClient(session_name, api_id, api_hash)
-chat = '@alfiron'
-async def main(chat):
-    async for message in client.iter_messages(chat, search= 'Украина'):
-        print(message.date, message.text)
-with client:
-    client.loop.run_until_complete(main(chat))
+chat_list = ['https://t.me/voenacher', '@ABUKADET']
+
+
+from_date = datetime.date(2022,3, 8)
+to_date = datetime.date(2022, 3, 11)
+date_list = []
+delta = to_date - from_date
+if delta.days <= 0:
+    print("Error")
+for i in range(delta.days + 1):
+    date = from_date + timedelta(i)
+    date_list.append(date)
+print(date_list)
+
+for date in date_list:
+    for chat in chat_list:
+        async def get_messages_at_date(chat, date):
+            result = []
+            tomorrow = date + datetime.timedelta(days=1)
+            async for msg in client.iter_messages(chat, offset_date=tomorrow):
+                edited_date = (msg.date).date()
+                if edited_date == date:
+                    one_message_info = [msg.sender.username, str(msg.date), msg.text]
+                    result.append(one_message_info)
+                else:
+                    return result
+
+        with client:
+            date_filt = client.loop.run_until_complete(get_messages_at_date(chat, date))
+            print(date_filt)
