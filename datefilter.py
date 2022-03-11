5e4b735eac84c36c8f44803ba7a5c3b6f65187ce
qTemiO <zhitihin.artem1441@yandex.ru>
2022-03-11 14:10:24 +0300

    Core done

--- datefilter.py
+++ datefilter.py
@@ -1,39 +1,38 @@
+from loguru import logger
 from telethon import TelegramClient
 import datetime
-from datetime import timedelta
 
 api_id = 15316406
 api_hash = 'ad62ac5526adf8d79b7d4b5d26f7e93e'
 session_name = 'Igor'
 client = TelegramClient(session_name, api_id, api_hash)
-chat_list = ['https://t.me/voenacher', '@alfiron']
+chat_list = ['https://t.me/voenacher']
 
 
-from_date = datetime.date(2022,3, 5)
-to_date = datetime.date(2022, 3, 11)
-date_list = []
-delta = to_date - from_date
-if delta.days <= 0:
-    print("Error")
-for i in range(delta.days + 1):
-    date = from_date + timedelta(i)
-    date_list.append(date)
-print(date_list)
+from_date = datetime.date(2022,3, 1)
+to_date = datetime.date(2022, 3, 7)
+search_key = 'Украина'
 
-for date in date_list:
+with open('result.txt', 'w') as f:
     for chat in chat_list:
-        async def get_messages_at_date(chat, date):
+        async def get_messages_at_date(chat, from_date, to_date, search_key):
             result = []
-            tomorrow = date + datetime.timedelta(days=1)
-            async for msg in client.iter_messages(chat, offset_date=tomorrow, search= 'Украина'):
-                edited_date = (msg.date).date()
-                if edited_date == date:
-                    one_message_info = [msg.sender.username, str(msg.date), msg.text]
-                    result.append(one_message_info)
-                else:
+            limit_day = to_date + datetime.timedelta(days=1)
+            lower_day = from_date - datetime.timedelta(days=1)
+            iterator = client.iter_messages(chat, offset_date=limit_day, search=search_key)
+            async for message in iterator:
+                if message.date.day == lower_day.day and message.date.month == lower_day.month and message.date.year == lower_day.year:
                     return result
 
+                one_message_info = [message.sender.username, message.date, message.text]
+                result.append(one_message_info)
+                
+                logger.debug(message.date)
+                logger.debug(from_date)
+
+
         with client:
-            date_filt = client.loop.run_until_complete(get_messages_at_date(chat, date))
-            print(date_filt)
-            print('ebat')
+            date_filt = client.loop.run_until_complete(get_messages_at_date(chat, from_date, to_date, search_key))
+            if date_filt != None:
+                for item in date_filt:
+                    f.write(f'{item[0]} {item[1]}\n')
